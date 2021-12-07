2f8fc2d44b7a7de6962bd325e03ed41808eae3c7
Jago de Vreede <jagodevreede@MacBook-Pro.local>
2021-12-07 20:04:31 +0100

    Added gpu profile

--- aws/before.sh
+++ aws/before.sh
@@ -2,7 +2,7 @@
 
 # This scripts runs on the local machine, after the instance is running, but before the ontarget.sh
 
-tar -zcvf training_data.tar.gz ../povray
+tar -zcvf training_data.tar.gz ../povray/bricks
 tar --exclude "../target" --exclude "../povray" --exclude "../lego_model.zip" --exclude "../aws" -czvf code.tar.gz ../
 
 scp -oStrictHostKeyChecking=no code.tar.gz $USER_NAME@$HOSTNAME:./
