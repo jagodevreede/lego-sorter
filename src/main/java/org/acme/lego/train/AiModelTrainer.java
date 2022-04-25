43a606c85f28e1b0b8cde67afac039f2f0d18a3e
Jago de Vreede <jagodevreede@MacBook-Pro.local>
2022-04-25 21:00:39 +0200

    Added resnet trainer

--- src/main/java/org/acme/lego/train/AiModelTrainer.java
+++ src/main/java/org/acme/lego/train/AiModelTrainer.java
@@ -2,6 +2,7 @@ package org.acme.lego.train;
 
 import org.deeplearning4j.core.storage.StatsStorage;
 import org.deeplearning4j.nn.conf.distribution.NormalDistribution;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.transferlearning.FineTuneConfiguration;
@@ -10,10 +11,12 @@ import org.deeplearning4j.ui.api.UIServer;
 import org.deeplearning4j.ui.model.stats.StatsListener;
 import org.deeplearning4j.ui.model.storage.InMemoryStatsStorage;
 import org.deeplearning4j.zoo.ZooModel;
+import org.deeplearning4j.zoo.model.ResNet50;
 import org.deeplearning4j.zoo.model.VGG16;
 import org.nd4j.evaluation.classification.Evaluation;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.learning.config.Adam;
 import org.nd4j.linalg.learning.config.Nesterovs;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.slf4j.Logger;
@@ -31,14 +34,13 @@ public class AiModelTrainer {
     protected static final long seed = 12345;
 
     private static final int trainPerc = 80;
-    private static final int batchSize = 16;
-    private static final String featureExtractionLayer = "fc2";
+    private static final int batchSize = 32;
+    private static final int trainForIter = 1000;
 
-    public static void main(String[] args) throws IOException {
-        log.info("Starting Ai model trainer for lego with {} bricks", numClasses);
-        final long startTime = System.currentTimeMillis();
-
+    private static ComputationGraph createVgg16Model() throws IOException {
+        final String featureExtractionLayer = "fc2";
         ZooModel zooModel = VGG16.builder().build();
+
         ComputationGraph vgg16 = (ComputationGraph) zooModel.initPretrained();
         log.info(vgg16.summary());
 
@@ -62,14 +64,51 @@ public class AiModelTrainer {
                                 .activation(Activation.SOFTMAX).build(),
                         "fc2")
                 .build();
-        log.info(vgg16Transfer.summary());
+        return vgg16Transfer;
+    }
+
+    private static ComputationGraph createResNet() throws IOException {
+        ZooModel zooModel = ResNet50.builder().build();
+        ComputationGraph resnet = (ComputationGraph) zooModel.initPretrained();
+
+        FineTuneConfiguration fineTuneConf = new FineTuneConfiguration.Builder()
+                .updater(new Adam(1e-3))
+                .seed(seed)
+                .build();
+
+        ComputationGraph resnet50Transfer = new TransferLearning.GraphBuilder(resnet)
+                .fineTuneConfiguration(fineTuneConf)
+                .setFeatureExtractor("bn5b_branch2c") //"block5_pool" and below are frozen
+                .addLayer("fc", new DenseLayer
+                        .Builder().activation(Activation.RELU).nIn(1000).nOut(256).build(), "fc1000") //add in a new dense layer
+                .addLayer("newpredictions", new OutputLayer
+                        .Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .activation(Activation.SOFTMAX)
+                        .nIn(256)
+                        .nOut(numClasses)
+                        .build(), "fc") //add in a final output dense layer,
+                // configurations on a new layer here will be override the finetune confs.
+                // For eg. activation function will be softmax not RELU
+                .setOutputs("newpredictions") //since we removed the output vertex and it's connections we need to specify outputs for the graph
+                .build();
+
+        return resnet50Transfer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        log.info("Starting Ai model trainer for lego with {} bricks", numClasses);
+        final long startTime = System.currentTimeMillis();
+
+        //ComputationGraph transferGraph = createVgg16Model();
+        ComputationGraph transferGraph = createResNet();
+        log.info(transferGraph.summary());
 
         UIServer uiServer = UIServer.getInstance();
         StatsStorage statsStorage = new InMemoryStatsStorage();
         uiServer.attach(statsStorage);
 
         //Then add the StatsListener to collect this information from the network, as it trains
-        vgg16Transfer.setListeners(new StatsListener(statsStorage));
+        transferGraph.setListeners(new StatsListener(statsStorage));
 
         log.info("Model setup complete");
 
@@ -85,9 +124,9 @@ public class AiModelTrainer {
         log.info("Labels exported");
 
         Evaluation eval;
-        eval = vgg16Transfer.evaluate(testIter);
+        /*eval = transferGraph.evaluate(testIter);
         log.info("Eval stats BEFORE fit.....");
-        log.info(eval.stats() + "\n");
+        log.info(eval.stats() + "\n"); */
         testIter.reset();
 
         int totalIters = 0;
@@ -98,21 +137,33 @@ public class AiModelTrainer {
         trainIter.reset();
         log.info("Total train iters: " + totalIters);
 
-        int iter = 0;
-        while (trainIter.hasNext()) {
-            log.info("Start train iter " + iter + "/" + totalIters + "....");
-            vgg16Transfer.fit(trainIter.next());
-            if (iter % 10 == 0) {
-                log.info("Evaluate model at iter " + iter + "/" + totalIters + " ....  in " + (System.currentTimeMillis() - startTime) / 1000 + "sec");
-                eval = vgg16Transfer.evaluate(testIter);
-                log.info(eval.stats());
-                testIter.reset();
+        int trainedIters = 0;
+        int e = 1;
+        while (trainedIters <= trainForIter) {
+            int iter = 0;
+            while (trainIter.hasNext()) {
+                log.info(e + " Start train iter " + iter + "/" + totalIters + ".... score: " + transferGraph.score());
+                transferGraph.fit(trainIter.next());
+//            if (iter % 10 == 0) {
+//                log.info("Evaluate model score " + transferGraph.score() + " at iter " + iter + "/" + totalIters + " ....  in " + (System.currentTimeMillis() - startTime) / 1000 + "sec");
+//                eval = transferGraph.evaluate(testIter);
+//                log.info(eval.stats());
+//                testIter.reset();
+//            }
+                iter++;
+                trainedIters++;
             }
-            iter++;
+            log.info(e + " Evaluate model score " + transferGraph.score() + " at iter " + iter + "/" + totalIters + " ....  in " + (System.currentTimeMillis() - startTime) / 1000 + "sec");
+            eval = transferGraph.evaluate(testIter);
+            log.info(eval.stats());
+            testIter.reset();
+            trainIter.reset();
+            transferGraph.save(new File("lego_model_e" + e + ".zip"));
+            e++;
         }
 
         log.info("Model build complete in " + (System.currentTimeMillis() - startTime) / 1000 + "sec");
-        vgg16Transfer.save(new File("lego_model.zip"));
+        transferGraph.save(new File("lego_model.zip"));
     }
 
     private static void exportLabels(DataSetIterator trainIter) throws IOException {
