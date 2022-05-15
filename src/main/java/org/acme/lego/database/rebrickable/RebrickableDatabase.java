e3bbec58e09ca115b475bff25902603051a9a1d5
Jago de Vreede <jagodevreede@MacBook-Pro.local>
2022-05-15 21:20:58 +0200

    Some queries on the Rebrickable Database in own db

--- src/main/java/org/acme/lego/database/rebrickable/RebrickableDatabase.java
+++ src/main/java/org/acme/lego/database/rebrickable/RebrickableDatabase.java
@@ -6,7 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import java.io.File;
+import java.util.List;
 
 @Slf4j
 @ApplicationScoped
@@ -16,9 +18,24 @@ public class RebrickableDatabase {
     @Inject
     RebrickableDatabaseInitializer databaseInitializer;
 
+    @Inject
+    EntityManager entityManager;
+
     // startup event
     void onStart(@Observes StartupEvent ev) {
         new RebrickableDownloader(dbFolder).download();
         databaseInitializer.initialize(dbFolder);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<String> getPopularParts() {
+        return entityManager.createNativeQuery("""
+                select s.partnum from (select partnum, sum(quantity) as total, count(inventoryid) as sets from inventoryparts i
+                group by partnum) as s
+                where s.total > 280
+                order by s.sets desc
+                """, String.class)
+                .setMaxResults(1000)
+                .getResultList();
     }
 }
