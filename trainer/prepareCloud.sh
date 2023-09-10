c76201184cd2b35b501f4fd6540ede525d3af7ec
Jago de Vreede <jagodevreede@gmail.com>
2023-09-10 19:28:50 +0200

    Fixes in cloud script

--- trainer/prepareCloud.sh
+++ trainer/prepareCloud.sh
@@ -7,11 +7,12 @@ if [ "$#" -ne 1 ]; then
 fi
 
 if [[ ! -f training_data.tar.gz ]]; then
-  tar -zcvf training_data.tar.gz -C ../povray/cropped .
+  echo "Compressing data"
+  tar  --no-xattrs -cf  training_data.tar.gz -C ../povray/cropped .
 fi
 
-ssh $USERNAME@$1 'mkdir -p lego/povray/cropped'
+ssh $USERNAME@$1 'rm -rf lego/povray/cropped; mkdir -p lego/povray/cropped'
 scp training_data.tar.gz $USERNAME@$1:./lego/training_data.tar.gz
 
 ssh $USERNAME@$1 'tar -xf lego/training_data.tar.gz -C ./lego/povray/cropped'
-ssh $USERNAME@$1 'rm lego/training_data.tar.gz'
+ssh $USERNAME@$1 'rm lego/training_data.tar.gz; find "./lego/povray/" -type f -name ".*" -delete'
--- trainer/runOnCloud.sh
+++ trainer/runOnCloud.sh
@@ -14,6 +14,6 @@ scp target/trainer-1.0.0-SNAPSHOT-shaded.jar $USERNAME@$1:./lego/trainer-1.0.0-S
 ssh $USERNAME@$1 'killall -9 java; rm -rf lego/model'
 
 # node manager and gpu node exporter are running on port 9100 and 9835 on the remote machine, map them to local host so that we can always scrape the same ip (localhost)
-ssh -L localhost:9101:localhost:9100 -L localhost:9835:localhost:9835 $USERNAME@$1 'cd lego; /home/$USERNAME/.sdkman/candidates/java/17.0.3.6.1-amzn/bin/java -jar trainer-1.0.0-SNAPSHOT-shaded.jar'
+ssh -L localhost:9101:localhost:9100 -L localhost:9835:localhost:9835 $USERNAME@$1 'cd lego; /home/ubuntu/.sdkman/candidates/java/17.0.3.6.1-amzn/bin/java -jar trainer-1.0.0-SNAPSHOT-shaded.jar'
 
 ./getResultsFromCloud.sh $1
